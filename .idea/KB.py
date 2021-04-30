c0b9c7d3399bb259f721b19ef15321ca41afeab5
iskineseren <s185157@student.dtu.dk>
2021-04-30 14:40:04 +0200

    tell and contraction 1.0

--- .idea/KB.py
+++ .idea/KB.py
@@ -1,10 +1,38 @@
 
 class KB:
 
+    KB=['r'];
 
     def KBAgent(kb):
+        kb
+
+    def tell(self):
+        b = input('Input new Belief : ')
+        print('this is your input: ', b)
+        i = []
+        i = b.split(',')
+        print('input array is', i)
+        return i
+
+    def ask(self):
+        kb= KB()
+        p= kb.tell() #Run tell method and get input from user
+        k = KB.KB #knowledgebase attribute
+        i=0
+        for i in p:
+            if i in k:
+                k.index(i)
+                print('Belief: ',i, 'is already in KB, do nothing')
+            else:
+                k.append(i)
+                print('belief added to KB: ',i, 'New updated KB', k)
 
 
-        def program(percept):
-            t=next(steps)
-            kb.tell(make)
+    def contraction(self,newbelief,index, knowledgebase):
+        knowledgebase.remove(index)
+        knowledgebase.append(newbelief)
+
+
+
+B=KB()
+B.ask()
